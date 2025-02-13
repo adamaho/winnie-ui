57fe5344efab1d823f466e09b32d188e5fd00530
adamaho <aho.adam01@gmail.com>
2025-02-13 15:59:23 -0500

    fix build

--- apps/docs/src/content/css/primitives/_components/listbox/listbox-with-checkbox.tsx
+++ apps/docs/src/content/css/primitives/_components/listbox/listbox-with-checkbox.tsx
@@ -1,5 +1,3 @@
-import { Checkbox } from "../checkbox/checkbox";
-
 export function ListboxWithCheckbox() {
   return (
     <div className="demo-listbox-container">
@@ -8,23 +6,18 @@ export function ListboxWithCheckbox() {
         className="demo-listbox not-content demo-indicator"
       >
         <li data-component="listbox-item">
-          <Checkbox data-slot="indicator" />
           <span data-slot="label">Apple</span>
         </li>
         <li data-component="listbox-item">
-          <Checkbox data-slot="indicator" />
           <span data-slot="label">Banana</span>
         </li>
         <li data-component="listbox-item">
-          <Checkbox data-slot="indicator" />
           <span data-slot="label">Cherry</span>
         </li>
         <li data-component="listbox-item">
-          <Checkbox data-slot="indicator" />
           <span data-slot="label">Date</span>
         </li>
         <li data-component="listbox-item">
-          <Checkbox data-slot="indicator" />
           <span data-slot="label">Elderberry</span>
         </li>
       </ul>
