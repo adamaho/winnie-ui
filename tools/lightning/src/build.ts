d1f8afb81f0fc9fba625c9b2b81c1d34d7a77b2d
adamaho <aho.adam01@gmail.com>
2025-03-16 16:46:36 -0400

    create css build script

---
+++ tools/lightning/src/build.ts
@@ -0,0 +1,64 @@
+import { Glob } from "bun";
+
+import browserslist from "browserslist";
+import { browserslistToTargets, bundle } from "lightningcss";
+
+type BuildCssOptions = {
+  /**
+   * directory to bundle css files from
+   */
+  inDir: string;
+
+  /**
+   * directory to place css file in
+   */
+  outDir: string;
+
+  /**
+   * browserslist targets
+   */
+  targets?: string;
+};
+
+/**
+ * Builds each file matching the glob into its own file
+ *
+ * @param options options available on the build
+ */
+async function buildCss({
+  inDir,
+  outDir,
+  targets = "last 2 versions",
+}: BuildCssOptions) {
+  const t = browserslistToTargets(browserslist(targets));
+  const glob = new Glob(`**/*.css`);
+
+  /**
+   * Collect files to bundle
+   */
+  const filesToBundle: string[][] = [];
+  try {
+    for await (const file of glob.scan(inDir)) {
+      const outFile = file.split(".")[0];
+      filesToBundle.push([`${inDir}/${file}`, `${outDir}/${outFile}.min.css`]);
+    }
+  } catch (error) {
+    console.log("Failed to parse glob file", error);
+    return;
+  }
+
+  /**
+   * Bundle files synchronously and write them to outDir
+   */
+  for (const [inFile, outFile] of filesToBundle) {
+    const { code } = bundle({ filename: inFile, minify: true, targets: t });
+    await Bun.write(outFile, code);
+    console.log(`bundled ${inFile} and wrote it to ${outFile}`);
+  }
+}
+
+buildCss({
+  inDir: "../../core/css/src",
+  outDir: "../../core/css/dist",
+  targets: "last 2 versions",
+}).catch((error) => console.log(error));
