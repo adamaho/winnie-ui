f752e8640bc512ea0d6bbcd15bbfebe0261d0a47
adamaho <aho.adam01@gmail.com>
2025-04-22 21:46:11 -0400

    my new favourite color

---
+++ apps/docs/src/pages/_controls.tsx
@@ -0,0 +1,47 @@
+import { useControls } from "leva";
+
+export function Controls() {
+  useControls({
+    "data-accent-color": {
+      options: [
+        "salmon",
+        "tomato",
+        "apricot",
+        "tuscon",
+        "mustard",
+        "caramel",
+        "lime",
+        "olive",
+        "grass",
+        "basil",
+        "peppermint",
+        "pine",
+        "coast",
+        "emerald",
+        "cyan",
+        "lagoon",
+        "sky",
+      ],
+      value: "salmon",
+      onChange(color) {
+        const layoutRoot = document.getElementById("playground")!;
+        layoutRoot.setAttribute("data-accent-color", color);
+      },
+    },
+    grey: {
+      value: false,
+      onChange(value) {
+        const layoutRoot = document.getElementById("playground")!;
+        if (value) {
+          layoutRoot.classList.add("grey");
+          layoutRoot.classList.remove("color");
+        } else {
+          layoutRoot.classList.add("color");
+          layoutRoot.classList.remove("grey");
+        }
+      },
+    },
+  });
+
+  return null;
+}
